8132d653a58c91ea511dada213370c17ac5f9f87
ewhitebloom <ewhitebloom@gmail.com>
2014-03-20 17:15:56 -0400

    ?

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,9 @@ class CommentsController < ApplicationController
   def create
     @micropost = Micropost.find(params[:micropost_id])
     @comment = @micropost.comments.create(params[:comment])
-    redirect_to micropost_path(@micropost)
+    if @comment.save
+      redirect_to micropost_path(@micropost)
+    end
   end
 
   private
